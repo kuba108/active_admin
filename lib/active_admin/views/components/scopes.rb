14e17d3e44fed31b6fc2817d65a79d9eafae45a9
Kuba <muslimixit@gmail.com>
2013-09-10 08:43:01 +0200

    test

--- lib/active_admin/views/components/scopes.rb
+++ lib/active_admin/views/components/scopes.rb
@@ -36,16 +36,8 @@ module ActiveAdmin
       protected
 
       def build_scope(scope, options)
-        li :class => classes_for_scope(scope) do
           scope_name = I18n.t("active_admin.scopes.#{scope.id}", :default => scope.name)
-
-          a :href => url_for(params.merge(:scope => scope.id, :page => 1)), :class => "table_tools_button" do
-            text_node scope_name
-            span :class => 'count' do
-              "(#{get_scope_count(scope)})"
-            end if options[:scope_count] && scope.show_count
-          end
-        end
+          item scope_name, "#", options
       end
 
       def classes_for_scope(scope)
